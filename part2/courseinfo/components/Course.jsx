1b2ab3c1429bd0dfa19678d29b9ecc18ccb5242c
Miqueas <miqueasgigena18@gmail.com>
2025-07-09 23:07:47 -0300

    Exercises 2.4* and 2.5*. Removed separate modules and integrated all subcomponents into the Course module.

--- part2/courseinfo/components/Course.jsx
+++ part2/courseinfo/components/Course.jsx
@@ -1,40 +1,83 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
+const Header = ({course}) => <h1>{course}</h1>
+
+const Total = ({courses}) => {
+    const initialValue = 0
+    const total = courses.parts.reduce((accumulator, part) => {
+        console.log('What is happening ', accumulator, part)
+        return accumulator + part.exercises
+    }, initialValue)
+
+    return (
+        <b> <p>total of {total} exercises</p> </b>
+    )
+}
+
+const Content = ({parts}) => {
+    console.log("parts", parts)
+    return (
+        <div>
+            {parts.map(part => <Part key={part.id} part={part} />)}
+        </div>
+    )
+}
+
+const Part = ({part}) => <p> {part.name} {part.exercises} </p>
+
+
 
 const Course = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
+    const courses = [
         {
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+            {
             name: 'Fundamentals of React',
             exercises: 10,
             id: 1
-        },
-        {
+            },
+            {
             name: 'Using props to pass data',
             exercises: 7,
             id: 2
-        },
-        {
+            },
+            {
             name: 'State of a component',
             exercises: 14,
             id: 3
-        },
-        {
-            name: 'redux',
+            },
+            {
+            name: 'Redux',
             exercises: 11,
             id: 4
-        }
+            }
         ]
-    }
+        }, 
+        {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+            {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+            },
+            {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+            }
+        ]
+        }
+    ]
 
     return (
         <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts}/>
+            <Header course={courses.name} />
+            <Content parts={courses[0].parts} />
+            <Total courses={courses[0]} />
+            <Content parts={courses[1].parts} />
+            <Total courses={courses[1]} />
         </div>
     )
 }
