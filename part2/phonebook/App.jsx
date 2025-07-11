dc09fe045d9acb82620a0229c1280a3af593ad2f
Miqueas <miqueasgigena18@gmail.com>
2025-07-11 15:54:51 -0300

    Exercise 2.11

--- part2/phonebook/App.jsx
+++ part2/phonebook/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({searchTerm, handleSearchTerm}) => (
     <div>filter shown with <input value={searchTerm} onChange={handleSearchTerm}/> </div>
@@ -31,16 +32,19 @@ const Persons = ({persons, newName, searchTerm}) => {
 
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [searchTerm, setSearchTerm] = useState('')
 
+    useEffect(() => {
+        console.log('effect')
+        axios.get('http://localhost:3001/persons').then(response => {
+            console.log('promise fulfilled')
+            setPersons(response.data)
+        })
+    }, [])
+    console.log('render', persons.length, 'persons')
 
     const handleNameChange = (event) => {
         console.log(event.target.value)
