bc18620b43648cf3b47dbf5b068ff2cadd93d139
Miqueas <miqueasgigena18@gmail.com>
2025-07-18 17:07:32 -0300

    Exercise 2.19* the key was to add another state, update the selected country in the dataCountry handler, and finally show the details in the main return

--- part2/dataCountries/App.jsx
+++ part2/dataCountries/App.jsx
@@ -4,7 +4,8 @@ import axios from 'axios'
 const App = () => {
     const [name, setName] = useState('')
     const [countries, setCountries] = useState([])
-    
+    const [selectedCountry, setSelectedCountry] = useState(null) // null is falsy, so the country details will show only after selection of a country
+
     useEffect(() => {
         console.log('effect')
         axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`).then(response => {
@@ -19,25 +20,33 @@ const App = () => {
         setName(event.target.value)
     }
 
+    const dataCountry = (cca3) => {
+        const foundCountry = countries.find(country => country.cca3 === cca3) 
+        
+        console.log("found country", foundCountry)
+        console.log('filter countries', filterCountries)
+        setSelectedCountry(foundCountry)
+    }
 
     const filterCountries = countries.filter(c => c.name.common.toLowerCase().includes(name.toLowerCase()))
 
     return (
         <div>
             <p>find countries <input value={name} onChange={handleChange}/></p>
-            {filterCountries.length === 1 
-                ? filterCountries.map(country => 
-                    <div key={country.cca3}>
-                        <h1>{country.name.common}</h1> 
-                        <p>Capital: {country.capital}</p>
-                        <p>Area: {country.area}</p>
+            {filterCountries.length  >  10 ? <p>Too many matches, specify another filter</p> 
+                                            : filterCountries.map(country => <p key={country.cca3}>{country.name.common} <button onClick={() => dataCountry(country.cca3)}>show</button></p>)}
+
+            {selectedCountry && (
+                    <div>
+                        <h1>{selectedCountry.name.common}</h1> 
+                        <p>Capital: {selectedCountry.capital}</p>
+                        <p>Area: {selectedCountry.area}</p>
                         <h2>Languages</h2>
                         <ul>
-                            {Object.values(country.languages).map((lang, index) => <li key={index}>{lang}</li>)} {/*learned the Object.values() method*/}
+                            {Object.values(selectedCountry.languages).map((lang, index) => <li key={index}>{lang}</li>)} {/*learned the Object.values() method*/}
                         </ul> 
-                        <img src = {country.flags.png}/>
-                    </div>) 
-                : filterCountries.length > 10 ? <p>Too many matches, specify another filter</p> : filterCountries.map(country => <p key={country.cca3}>{country.name.common}</p>)}
+                        <img src={selectedCountry.flags.png}/>
+                    </div>)}
         </div>
     )
 }
