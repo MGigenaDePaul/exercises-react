ed0f7408d7ff5782107db6a1484bc72574ede094
Miqueas <miqueasgigena18@gmail.com>
2025-07-20 14:13:55 -0300

    Exercise 2.20*, PART 2 FINISHED

--- part2/dataCountries/App.jsx
+++ part2/dataCountries/App.jsx
@@ -5,10 +5,13 @@ const App = () => {
     const [name, setName] = useState('')
     const [countries, setCountries] = useState([])
     const [selectedCountry, setSelectedCountry] = useState(null) // null is falsy, so the country details will show only after selection of a country
+    const [weather, setWeather] = useState([])
+
+    const GET_COUNTRIES = `https://studies.cs.helsinki.fi/restcountries/api/all`
 
     useEffect(() => {
-        console.log('effect')
-        axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`).then(response => {
+        console.log('effect for countries')
+        axios.get(GET_COUNTRIES).then(response => {
             setCountries(response.data)
         })
         .catch(() => {
@@ -16,6 +19,29 @@ const App = () => {
         })
     }, [])
 
+    
+    const api_key = import.meta.env.VITE_SOME_KEY
+    console.log("api key:", api_key)
+
+    useEffect(() => {
+        console.log('effect for weathers')
+        
+        if (!selectedCountry || selectedCountry.capitalInfo.latlng.length < 2){
+            console.log('Selected Country does not exist', selectedCountry)
+            return;
+        } 
+
+        const GET_WEATHER = `https://api.openweathermap.org/data/2.5/weather?lat=${selectedCountry.capitalInfo.latlng[0]}&lon=${selectedCountry.capitalInfo.latlng[1]}&appid=${api_key}`
+
+        axios.get(GET_WEATHER).then(response => {
+            setWeather(response.data)
+            console.log(response.data)
+        })
+        .catch(() => {
+            console.log('Could not fetch weather')
+        })  
+    }, [selectedCountry])
+    
     const handleChange = (event) => {
         setName(event.target.value)
     }
@@ -37,13 +63,30 @@ const App = () => {
             {selectedCountry && (
                     <div>
                         <h1>{selectedCountry.name.common}</h1> 
-                        <p>Capital: {selectedCountry.capital}</p>
-                        <p>Area: {selectedCountry.area}</p>
+                        <p>Capital {selectedCountry.capital}</p>
+                        <p>Area {selectedCountry.area}</p>
                         <h2>Languages</h2>
                         <ul>
-                            {Object.values(selectedCountry.languages).map((lang, index) => <li key={index}>{lang}</li>)} {/*learned the Object.values() method*/}
+                            {Object.values(selectedCountry.languages).map((lang, index) => <li key={index}>{lang}</li>)} 
                         </ul> 
                         <img src={selectedCountry.flags.png} alt={`flag of ${selectedCountry.name.common}`}/>
+                        <h2>Weather in {selectedCountry.capital}</h2>
+                        {weather && (
+                            <div> 
+                                {weather?.main
+                                    ? <p>Temperature {(weather.main.temp - 273.15).toFixed(2)} Celsius</p> // Celsius = Kelvin - 273.15 //
+                                    : <p>Loading temperature...</p>
+                                }
+                                {weather.weather?.[0].icon 
+                                    ? <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                                    : <p>Loading icon...</p>
+                                }
+                                {weather?.wind?.speed
+                                    ? <p>Wind {weather.wind.speed} m/s</p> // gust does not appear much, so it's better to show the wind speed
+                                    : <p>Loading wind...</p>
+                                }
+                            </div>
+                        )}
                     </div>)}
         </div>
     )
