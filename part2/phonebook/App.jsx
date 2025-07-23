2d71675b4f3046274b174a716b543a28a4cc381e
Miqueas <miqueasgigena18@gmail.com>
2025-07-23 17:54:55 -0300

    made cleaner phonebook folder of part 2, now the src folder will be the frontend and part3 the backend

--- part2/phonebook/App.jsx
+++ part2/phonebook/App.jsx
@@ -1,30 +1,10 @@
 import { useState, useEffect } from 'react'
 import personService from './services/persons'
 import Notification from './components/Notification'
+import Filter from './components/Filter' 
+import PersonForm from './components/PersonForm' 
+import Persons from './components/Persons'
 import './index.css'
-
-const Filter = ({searchTerm, handleSearchTerm}) => (
-    <div>filter shown with <input value={searchTerm} onChange={handleSearchTerm}/> </div>
-)
-
-const PersonForm = ({addPerson, newName, handleNameChange, handleNumberChange, newNumber}) => (
-        <form onSubmit={addPerson}>
-            <div>name: <input value={newName} onChange={handleNameChange}/></div>
-            <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
-            <div><button type="submit">add</button></div>
-        </form>
-)
-
-const Persons = ({persons, searchTerm, deletePerson}) => {
-    const filterPeopleByName = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
-    console.log(filterPeopleByName)
-
-    return (
-        <div>
-            {filterPeopleByName.map(person => <li key={person.id}>{person.name} {person.number} <button onClick={() => deletePerson(person.id)}>delete</button></li>)}
-        </div>
-    )
-}
 
 
 const App = () => {
